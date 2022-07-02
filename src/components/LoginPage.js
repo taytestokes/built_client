72c7c6430996880b180fb68489165f7c2cf2ac80
taytestokes <taytestokes9328@gmail.com>
2022-07-02 17:31:27 -0600

    Uses roboto google font

---
+++ src/components/LoginPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styled from '@emotion/styled';
+
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+`;
+
+export const LoginPage = () => {
+  return <Container>This is the login page!</Container>;
+};
--- src/index.js
+++ src/index.js
@@ -1,4 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { css, Global, ThemeProvider } from '@emotion/react';
+import emotionReset from 'emotion-reset';
 
-ReactDOM.render(<h1>Hello World</h1>, document.getElementById('root'));
+import { LoginPage } from './components/LoginPage';
+
+ReactDOM.render(
+  <ThemeProvider theme={{}}>
+    <Global
+      styles={css`
+        @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700;900&display=swap');
+
+        ${emotionReset}
+
+        *, *::after, *::before {
+          box-sizing: border-box;
+          -moz-osx-font-smoothing: grayscale;
+          -webkit-font-smoothing: antialiased;
+          font-smoothing: antialiased;
+        }
+
+        html {
+          font-family: Roboto, sans-serif;
+        }
+      `}
+    />
+    <LoginPage />
+  </ThemeProvider>,
+  document.getElementById('root')
+);
